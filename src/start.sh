3fccc6f421bfbaead237645a7ed330738ce0651a
Sandy Corsillo <the_dusky@icloud.com>
2024-01-13 15:03:50 -0500

    ckpt 2.1 lora

--- src/start.sh
+++ src/start.sh
@@ -2,10 +2,10 @@
 
 echo "Worker Initiated"
 
-echo ls -la /workspace
+echo ls -la /runpod-volume
 
 echo "Starting WebUI API"
-python /stable-diffusion-webui/webui.py --skip-python-version-check --skip-torch-cuda-test --skip-install --ckpt /model.ckpt --lora-dir /workspace/lora --lowram --opt-sdp-no-mem-attention --disable-safe-unpickle --port 3000 --api --nowebui --skip-version-check  --no-hashing --no-download-sd-model &
+python /stable-diffusion-webui/webui.py --skip-python-version-check --skip-torch-cuda-test --skip-install --ckpt /model.ckpt --lora-dir /runpod-volume/lora --lowram --opt-sdp-no-mem-attention --disable-safe-unpickle --port 3000 --api --nowebui --skip-version-check  --no-hashing --no-download-sd-model &
 
 echo "Starting RunPod Handler"
 python -u /rp_handler.py
