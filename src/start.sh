2ff9ff953acb0e87dd5d41b287cc106f5d7e5d71
Sandy Corsillo <the_dusky@icloud.com>
2024-01-14 16:53:05 -0500

    dynamic model try 8

--- src/start.sh
+++ src/start.sh
@@ -2,18 +2,12 @@
 
 echo "Worker Initiated"
 
-echo ls -la /
+ls -la /
 
-while getopts ":m" flag > /dev/null 2>&1
-do
-    case ${flag} in
-        m) model="${OPTARG}" ;;
-        *) break;; 
-    esac
-done
+echo $MODEL
 
 echo "Starting WebUI API"
-python /stable-diffusion-webui/webui.py --skip-python-version-check --skip-torch-cuda-test --skip-install --ckpt /${model} --lora-dir /runpod-volume/loras --lowram --opt-sdp-no-mem-attention --disable-safe-unpickle --port 3000 --api --nowebui --skip-version-check  --no-hashing --no-download-sd-model &
+python /stable-diffusion-webui/webui.py --skip-python-version-check --skip-torch-cuda-test --skip-install --ckpt /${MODEL} --lora-dir /runpod-volume/loras --lowram --opt-sdp-no-mem-attention --disable-safe-unpickle --port 3000 --api --nowebui --skip-version-check  --no-hashing --no-download-sd-model &
 
 echo "Starting RunPod Handler"
 python -u /rp_handler.py
