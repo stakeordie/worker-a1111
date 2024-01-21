1d2298301b1d30f2947d0d19b696350f2b9189ab
Sandy Corsillo <the_dusky@icloud.com>
2024-01-21 02:59:29 -0500

    no-half-vae 5

--- src/start.sh
+++ src/start.sh
@@ -16,7 +16,7 @@ echo $HALF
 echo $LORA
 
 echo "Starting WebUI API"
-python /stable-diffusion-webui/webui.py --skip-python-version-check --skip-torch-cuda-test --skip-install --ckpt /${MODEL} $LORA --lowram --opt-sdp-no-mem-attention --disable-safe-unpickle --port 3000 --api --nowebui --skip-version-check --no-hashing --no-download-sd-model ${HALF} &
+python /stable-diffusion-webui/webui.py --skip-python-version-check --skip-torch-cuda-test --skip-install --ckpt /${MODEL} $LORA --opt-sdp-no-mem-attention --disable-safe-unpickle --port 3000 --api --nowebui --skip-version-check --no-download-sd-model ${HALF} &
 
 echo "Starting RunPod Handler"
 python -u /rp_handler.py
