a1ec9b16c6d06dcbca5309cb85faf68ab6d2a19e
Sandy Corsillo <the_dusky@icloud.com>
2024-01-22 15:35:06 -0500

    persist for testing 6

--- src/start.sh
+++ src/start.sh
@@ -19,4 +19,8 @@ echo "Starting WebUI API"
 
 python /stable-diffusion-webui/webui.py --skip-python-version-check --skip-torch-cuda-test --skip-install --ckpt /${MODEL} $LORA --opt-sdp-no-mem-attention --disable-safe-unpickle --port 3000 --api --nowebui --skip-version-check --no-download-sd-model ${HALF} &
 echo "Starting RunPod Handler"
-python -u /rp_handler.py --test_input '{"input": {"type":"txt2img","prompt": {"prompt": "The quick brown fox jumps"}}}'
+
+if(1==0) {
+python -u /rp_handler.py
+} else {
+}
