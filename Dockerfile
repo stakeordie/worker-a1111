32ea05d3b7befe311b6cd76bb0b6f206b0ae5cde
Sandy Corsillo <the_dusky@icloud.com>
2024-01-22 13:53:48 -0500

    persist for testing 3

--- Dockerfile
+++ Dockerfile
@@ -39,7 +39,6 @@ ARG SHA=5ef669de080814067961f28357256e8fe27544f4
 ARG model
 ARG half=--no-half-vae
 ARG lora=--lora-dir /runpod-volume/loras
-ARG persist="false"
 RUN echo "HALF = $half"
 RUN echo "LORA = $lora"
 ENV DEBIAN_FRONTEND=noninteractive \
@@ -49,8 +48,7 @@ ENV DEBIAN_FRONTEND=noninteractive \
     PYTHONUNBUFFERED=1 \
     MODEL=${model} \
     HALF=${half} \
-    LORA=${lora} \
-    PERSIST=${persist}
+    LORA=${lora}
 
 SHELL ["/bin/bash", "-o", "pipefail", "-c"]
 
@@ -102,7 +100,4 @@ RUN apt-get autoremove -y && \
 
 # Set permissions and specify the command to run
 RUN chmod +x /start.sh
-ARG model
-ARG half=--no-half-vae
-ARG lora
 CMD /start.sh
--- src/start.sh
+++ src/start.sh
@@ -16,9 +16,7 @@ echo $HALF
 echo $LORA
 
 echo "Starting WebUI API"
-python /stable-diffusion-webui/webui.py --skip-python-version-check --skip-torch-cuda-test --skip-install --ckpt /${MODEL} $LORA --opt-sdp-no-mem-attention --disable-safe-unpickle --port 3000 --api --nowebui --skip-version-check --no-download-sd-model ${HALF}
 
-if($PERSIST == "false") {
-  echo "Starting RunPod Handler"
-  python -u /rp_handler.py
-}
+python /stable-diffusion-webui/webui.py --skip-python-version-check --skip-torch-cuda-test --skip-install --ckpt /${MODEL} $LORA --opt-sdp-no-mem-attention --disable-safe-unpickle --port 3000 --api --nowebui --skip-version-check --no-download-sd-model ${HALF} &
+echo "Starting RunPod Handler"
+python -u /rp_handler.py
