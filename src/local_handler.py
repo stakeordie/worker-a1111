12f8e3dab623e01239d1150a8d32d47951e3956c
Sandy Corsillo <the_dusky@icloud.com>
2024-01-29 20:20:15 -0500

    img2img test 16

--- src/local_handler.py
+++ src/local_handler.py
@@ -40,11 +40,12 @@ def run_inference(inference_request):
     '''
 
     if(inference_request["type"] == "img2img"):
-        print("Pulling Image")
-        image = get_as_base64(inference_request["params"]["init_images"][0])
-        image_string = image.decode('UTF-8')
-        inference_request["params"]["init_images"][0] = image_string
-        print("Sending to API")
+        if("http" in inference_request["params"]["init_images"][0]):
+            print("Pulling Image")
+            image = get_as_base64(inference_request["params"]["init_images"][0])
+            image_string = image.decode('UTF-8')
+            inference_request["params"]["init_images"][0] = image_string
+            print("Sending to API")
         response = automatic_session.post(url=f'{LOCAL_URL}/img2img',
                                       json=inference_request["params"], timeout=600)
     elif(inference_request["type"] == "extra-single-image"):
