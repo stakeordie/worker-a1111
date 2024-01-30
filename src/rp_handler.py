0acdad7505d9d349deaf6de2b481ca37b417f7fb
Sandy Corsillo <the_dusky@icloud.com>
2024-01-30 00:15:02 -0500

    update docker file 7

--- src/rp_handler.py
+++ src/rp_handler.py
@@ -1,5 +1,5 @@
 import time
-
+import base64
 import runpod
 import requests
 from requests.adapters import HTTPAdapter, Retry
@@ -36,28 +36,28 @@ def run_inference(inference_request):
     Run inference on a request
     '''
 
+    if hasattr(inference_request, 'params'):
+        params = 'params'
+    else:
+        params = 'prompt'
+
     if(inference_request["type"] == "img2img"):
-        async function getImage(url: string) {
-            const response = await fetch(url);
-            if (!response.ok) throw new Error("Failed to fetch image");
-            const arrayBuffer = await response.arrayBuffer();
-            const buffer = Buffer.from(arrayBuffer);
-            return buffer.toString("base64");
-        }
-        print("Pulling Image")
-        const image = await getImage(inference_request["params"]["init_images"][0])
-        inference_request["params"]["images"][0] = image
-        print("Sending to API")
+        if("http" in inference_request[params]["init_images"][0]):
+            print("Pulling Image")
+            image = get_as_base64(inference_request[params]["init_images"][0])
+            image_string = image.decode('UTF-8')
+            inference_request[params]["init_images"][0] = image_string
+            print("Sending to API")
         response = automatic_session.post(url=f'{LOCAL_URL}/img2img',
-                                      json=inference_request["params"], timeout=600)
+                                      json=inference_request[params], timeout=600)
     elif(inference_request["type"] == "extra-single-image"):
         response = automatic_session.post(url=f'{LOCAL_URL}/extra-single-image',
-                                      json=inference_request["params"], timeout=600)
+                                      json=inference_request[params], timeout=600)
     else:
         response = automatic_session.post(url=f'{LOCAL_URL}/txt2img',
-                                      json=inference_request["params"], timeout=600)
+                                      json=inference_request[params], timeout=600)
 
-    # input_settings = inference_request["params"]
+    # input_settings = inference_request[params]
     # input_settings["image"] = ''
     # input_settings["images"] = ''
     # res_for_write = response.json()
--- src/rp_handler_upscaler.py
+++ src/rp_handler_upscaler.py
@@ -1,5 +1,5 @@
 import time
-
+import base64
 import runpod
 import requests
 from requests.adapters import HTTPAdapter, Retry
@@ -36,15 +36,26 @@ def run_inference(inference_request):
     Run inference on a request.
     '''
 
+    if hasattr(inference_request, 'params'):
+        params = 'params'
+    else:
+        params = 'prompt'
+
     if(inference_request["type"] == "img2img"):
+        if("http" in inference_request[params]["init_images"][0]):
+            print("Pulling Image")
+            image = get_as_base64(inference_request[params]["init_images"][0])
+            image_string = image.decode('UTF-8')
+            inference_request[params]["init_images"][0] = image_string
+            print("Sending to API")
         response = automatic_session.post(url=f'{LOCAL_URL}/img2img',
-                                      json=inference_request["prompt"], timeout=600)
+                                      json=inference_request[params], timeout=600)
     elif(inference_request["type"] == "extra-single-image"):
         response = automatic_session.post(url=f'{LOCAL_URL}/extra-single-image',
-                                      json=inference_request["prompt"], timeout=600)
+                                      json=inference_request[params], timeout=600)
     else:
         response = automatic_session.post(url=f'{LOCAL_URL}/txt2img',
-                                      json=inference_request["prompt"], timeout=600)
+                                      json=inference_request[params], timeout=600)
 
     return response.json()
 
