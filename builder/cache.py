d28e51c6e9f93e50dbc497e63a3f66273c0e3a48
Sandy Corsillo <the_dusky@icloud.com>
2024-01-30 16:12:04 -0500

    update dockerfile ????

--- builder/cache.py
+++ builder/cache.py
@@ -1,7 +1,9 @@
 from webui import initialize
 import modules.interrogate
+import modules.sd_models
 
 initialize.initialize()
 interrogator = modules.interrogate.InterrogateModels("interrogate")
 interrogator.load()
 interrogator.categories()
+sd_models.load('/2_1.ckpt')
